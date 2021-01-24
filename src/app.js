cc1211766acfa50c5296602c79f5e3f7b6510f80
Yngrid Coello <yngrdyn@gmail.com>
2021-01-24 12:19:41 +0100

    Setting up router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -9,4 +10,33 @@ const App = () => {
   )
 };
 
-ReactDOM.render(<App/>, document.getElementById('root-app'));
+const AddExpensePage = () => {
+  return (
+    <div title="keepit" subtitle="Keep your expenses within a budget">Create an expense</div>
+  )
+};
+
+const EditExpensePage = () => {
+  return (
+    <div title="keepit" subtitle="Keep your expenses within a budget">Edit an expense</div>
+  )
+};
+
+const HelpPage = () => {
+  return (
+    <div title="keepit" subtitle="Keep your expenses within a budget">How can we help?</div>
+  )
+};
+
+const routes = (
+  <div>
+    <BrowserRouter>
+      <Route path='/' component={App} exact={true} />
+      <Route path='/create' component={AddExpensePage} />
+      <Route path='/edit' component={EditExpensePage} />
+      <Route path='/help' component={HelpPage} />
+    </BrowserRouter>
+  </div>
+)
+
+ReactDOM.render(routes, document.getElementById('root-app'));
--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
   devtool: 'source-map',
   devServer: {
     contentBase: path.join(__dirname, '/public'),
-    publicPath: '/build'
+    publicPath: '/build',
+    historyApiFallback: true,
   }
 }
