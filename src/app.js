4eda124a1f89ef5cf4de33b57e567920e4646ad1
Yngrid Coello <yngrdyn@gmail.com>
2021-03-13 22:49:36 +0100

    Adding react-redux library + using Provider HOC to connect to redux store from the app

--- src/app.js
+++ src/app.js
@@ -2,13 +2,23 @@ import 'normalize.css/normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
-import { Store } from './store';
+import { addExpense, getVisibleExpenses, Store } from './store';
 import './styles/styles.scss';
+import { Provider } from 'react-redux';
 
 const store = Store();
 
-store.subscribe(() => {
-  console.log(visibleExpenses);
-});
+store.dispatch(addExpense({ description: 'Water bill'}));
+store.dispatch(addExpense({ description: 'Gas bill'}));
 
-ReactDOM.render(<AppRouter/>, document.getElementById('root-app'));
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('root-app'));
