f9489a2e439c3a728677d136ba208278673d3585
Yngrid Coello <yngrdyn@gmail.com>
2021-03-14 22:07:21 +0100

    Setting up redux devtools extension

--- src/store/Store.js
+++ src/store/Store.js
@@ -3,7 +3,8 @@ import reducers from './Reducers';
 
 export default () => {
   const store = createStore(
-    reducers
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   );
 
   return store;
