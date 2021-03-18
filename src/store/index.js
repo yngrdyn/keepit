35b64c26244143b413020107965452c78af786a2
Yngrid Coello <yngrdyn@gmail.com>
2021-03-18 22:19:34 +0100

    Reexporting actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,16 @@
+import { addExpense, editExpense, removeExpense, setEndDate, setStartDate, setTextFilter, sortByAmount, sortByDate } from './Actions';
 import { getVisibleExpenses } from './Selectors';
 import Store from './Store';
 
 export {
+  addExpense,
+  editExpense,
   getVisibleExpenses,
+  removeExpense,
+  setEndDate,
+  setStartDate,
+  setTextFilter,
+  sortByAmount,
+  sortByDate,
   Store,
 };
