8268b923a244373eee13ce66a304e6ff81064354
Yngrid Coello <yngrdyn@gmail.com>
2021-03-21 23:26:35 +0100

    Extracting css files into their own file

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: path.join(__dirname, 'src/app.js'),
@@ -8,9 +9,6 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: ''
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-  ],
   module: {
     rules: [
       {
@@ -25,11 +23,26 @@ module.exports = {
       {
         test: /\.s?css$/,
         use: [
-          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                return path.relative(path.dirname(resourcePath), context) + '/';
+              },
+            },
+          },
           'css-loader',
           'sass-loader',
         ],
       }
     ]
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false,
+    }),
+  ],
 }
