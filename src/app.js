e6a3074780bd192f7e24c150ac0b8f8ded74f589
Yngrid Coello <yngrdyn@gmail.com>
2021-03-21 21:58:14 +0100

    Removing dummy data

--- src/app.js
+++ src/app.js
@@ -4,14 +4,10 @@ import 'react-dates/lib/css/_datepicker.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import { addExpense, Store } from './store';
+import { Store } from './store';
 import './styles/styles.scss';
 
 const store = Store();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const jsx = (
   <Provider store={store}>
