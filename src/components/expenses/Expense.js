8eaa00c604106fc613d10b05dee5dd9e0b921237
Yngrid Coello <yngrdyn@gmail.com>
2021-03-24 21:31:42 +0100

    Formatting amount and date in expense component

--- src/components/expenses/Expense.js
+++ src/components/expenses/Expense.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const Expense = ({ id, description, amount, createdAt }) => (
     <div>
       <Link to={`edit/${id}`}>
         <h3> { description } </h3>
       </Link>
-      <div> { amount } -  { createdAt } </div>
+      <div> { numeral(amount/100).format('$0,0.00') } -  { moment(createdAt).format('MMM do, YYYY') } </div>
     </div>
   );
 
